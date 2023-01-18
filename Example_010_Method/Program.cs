d333b603ec69fef1d8a5d243856a3428cc1c7870
Zver-gus <keeper95555@gmail.com>
2023-01-18 21:32:57 +0400

    all Ex_010

---
+++ Example_010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array ={1,24,35,4,98,26,17,68,98};
+
+int n = array.Length;
+int find = 98;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
