3161fb737c7ac6e5eb340fc1056fa98ae5b4a604
Zver-gus <keeper95555@gmail.com>
2023-01-24 00:02:53 +0400

    add eminar_202

---
+++ Seminar_202/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+{
+    if (x % y == 0)
+    {
+        Console.WriteLine("Первое число кратно второму");
+    }
+    else
+    {
+        double result =  x % y;
+        Console.WriteLine("Не кратно, остаток равен " + result);
+    }
+}
+else
+{
+     if (y % x == 0)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        double result = y % x;
+        Console.WriteLine("Не кратно, остаток равен " + result);
+    }
+}
