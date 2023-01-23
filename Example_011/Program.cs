7a5f452a05901f58508afcf23bb964312eddbf7c
Zver-gus <keeper95555@gmail.com>
2023-01-23 21:51:58 +0400

    add eminar_01

--- Example_011/Program.cs
+++ Example_011/Program.cs
@@ -25,13 +25,14 @@ int IndexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position = 0;
+    int position = -1;
 
     while(index < count)
     {
         if (collection[index] == find)
         {
             position = index;
+            break;
         }
         index++;
     }
@@ -41,6 +42,8 @@ int IndexOf(int[] collection, int find)
 int [] array = new int[10];
 
 FillArray(array);
+// array[4] = 4;
+// array[6] = 4;
 PrintArray(array);
 Console.WriteLine();
 
---
+++ Seminar_002/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = new Random().Next(10, 99);
+int first = N / 10;
+int second = N % 10;
+if (first > second)
+{
+    Console.WriteLine(first);
+}
+else
+{
+    Console.WriteLine(second);
+}
