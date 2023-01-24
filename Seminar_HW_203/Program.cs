987745f65a307fd2d7c1944d7642ea3f134bd775
Zver-gus <keeper95555@gmail.com>
2023-01-24 23:38:53 +0400

    add seminar_hw_203

---
+++ Seminar_HW_203/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число для проверки кратности на 7 и 23 ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n);
+
+if ((n % 7 == 0) & (n % 23 == 0)) Console.WriteLine("Кратное");
+else Console.WriteLine("Не кратное");
