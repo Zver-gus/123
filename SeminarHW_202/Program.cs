6b721aff9bdddbb50c422ed6472e9caf1e4559b9
Zver-gus <keeper95555@gmail.com>
2023-01-24 20:00:47 +0400

    add seminar_hw_202

---
+++ SeminarHW_202/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = new Random().Next(1, 655);
+Console.WriteLine(n);
+
+if (n < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    
+}
+else
+{
+    while (n > 999) n = n / 10;
+    n = n % 10;
+    Console.WriteLine(n);
+}
+
