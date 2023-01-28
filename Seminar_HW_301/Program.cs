8d203a6e52eb6324d35c01fa22db1366d81bcce8
Zver-gus <keeper95555@gmail.com>
2023-01-28 20:57:12 +0400

    add seminar_hw_301

---
+++ Seminar_HW_301/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ведите длину массива ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    arr[i] = new Random().Next(1,11);
+    Console.Write(arr[i]+" ");
+}
+
+int summ = 0;
+int result = 0;
+int countOfEvens = 0;
+for (int i = 0; i < N; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        summ += arr[i];
+        countOfEvens++;
+    }
+}
