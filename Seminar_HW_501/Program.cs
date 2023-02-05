bc2b6dd9422804633d5030b4820b2c349fbbe01b
Zver-gus <keeper95555@gmail.com>
2023-02-05 23:39:29 +0400

    add seminar&lec

---
+++ Seminar_HW_501/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] FillArr (int len)
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(-101,101);
+        Console.Write(arr[i] + " ");
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите длину массива ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArr(len);
+
+int sum = 0;
+for (int i = 0; i<len; i++)
+{
+    if (i % 2 != 0)
+        sum = sum + array[i];
+}
+Console.WriteLine();
+Console.WriteLine(sum);
+
---
+++ Seminar_HW_502/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] FillArr (int len)
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(-101,101);
+        Console.Write(arr[i] + " ");
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите длину массива ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArr(len);
+
+int max = array[0];
+int min = array[0];
+for (int i = 0; i < len; i++)
+{
+    if (max < array[i])
+        max = array[i];
+    else if (min > array[i])
+        min = array[i];
+}
+
+int diff = max - min;
+
+Console.WriteLine();
+Console.Write(diff);
